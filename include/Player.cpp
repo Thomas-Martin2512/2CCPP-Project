639a3c06418fe490e28a83f1592259881f29b385
Ay0ko <128031560+Ay0ko@users.noreply.github.com>
2025-11-07 14:31:59 +0100

    Création class Player

---
+++ include/Player.cpp
@@ -0,0 +1,37 @@
+#include "Player.h"
+#include <iostream>
+
+Player::Player() = default;
+Player::Player(const std::string& playerName, const std::string& playerColor)
+    : name(playerName), color(playerColor) {}
+
+
+
+void Player::inputPlayerInfo(int playerNumber) {
+    std::cout << "=== Joueur " << playerNumber << " ===\n";
+    std::cout << "Entrez le nom du joueur : ";
+    std::getline(std::cin,name);
+    std::cout << "Entrez une couleur valide : ";
+    std::getline(std::cin,color);
+    std::cout << "Joueur " << name << "enregistré !" << std::endl;
+}
+
+void Player::displayInfo() const {
+    std::cout << "Name: " << name << ", couleur : "<< color << std::endl;
+}
+
+std :: string Player::getName() const {
+    return name;
+}
+
+std :: string Player::getColor() const {
+    return color;
+}
+
+void Player::setName(const std::string& newName) {
+    this->name = newName;
+}
+
+void Player::setColor(const std :: string & newColor) {
+    this->color = newColor;
+}
